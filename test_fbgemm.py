695326e1ae9031850757168d2d37472c58d28379
root <root@rtptest41418.prn2.facebook.com>
2019-10-31 13:18:54 -0700

    update fbgemm test

--- test_fbgemm.py
+++ test_fbgemm.py
@@ -15,25 +15,36 @@ def bench(p, n, m, k):
   sparsity = 1 / 1.5 ** p
   b = torch.tensor(scipy.sparse.random(k, m, density= sparsity).todense().astype(np.float32))
   print("{:.2f}% non-zero, n:{}xk:{}xm:{}".format(sparsity * 100, n, k, m))
-
+  flops = 2 * n * m * k
   iters = 1000
 
   c_fbg = torch.ops.sparse2.smm(a, b, cache_id)
   torch.testing.assert_allclose(c_fbg, a@b)
   c_t = F.linear(a, b.t())
   torch.testing.assert_allclose(c_fbg, c_t)
-  t = time.time()
+  t1_start = time.perf_counter()  
   for _ in range(iters):
     c = torch.ops.sparse2.smm(a, b, cache_id)
-  print(" ", time.time() - t)
-  b_t = b.t().contiguous()
-  t = time.time()
-  for _ in range(iters):
-    c = F.linear(a, b_t)
-  print(" ", time.time() - t)
+  d = time.perf_counter() - t1_start
+  s = flops / (d * 1e9) * iters
+  r = s * sparsity
+  print(" ", d, s, r)
 
-for p in range(1,10):
-  for n in [1]:
-    for m in [64, 128, 256]:
+  b_t = b.t().contiguous()
+  t1_start = time.perf_counter()  
+  for _ in range(iters):
+    #c = a @ b
+    c = F.linear(a, b_t)
+  d = time.perf_counter() - t1_start
+  s = flops / (d * 1e9) * iters
+  print(" ", d, s)
+  #print(" ", time.perf_counter() - t1_start)
+
+#n=m=k=1024
+#bench(5, n, m, k)
+#exit(0)
+for p in range(5, 10):
+  for n in [16, 64]:
+    for m in [64, 128]:
       for k in [128, 256]:
         bench(p, n, m, k)
